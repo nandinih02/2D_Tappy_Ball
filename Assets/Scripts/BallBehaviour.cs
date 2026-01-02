53e7395079cefb593be3e05045fcede35a780648
nandinih02 <79862681+nandinih02@users.noreply.github.com>
2026-01-02 13:37:58 -0500

    Added Ball Movement

---
+++ Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,51 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BallBehaviour : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float jumpForce;
+    public InputAction jump;
+    bool started;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        started = false;
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        
+    }
+
+    void OnEnable()
+    {
+        jump.Enable();
+            
+    }
+
+    void OnDisable()
+    {
+        jump.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!started)
+        {
+            if(jump.triggered)
+            {
+                started = true;
+                rb.bodyType = RigidbodyType2D.Dynamic;
+            }
+        }
+        else
+        {
+            if(jump.triggered)
+            {
+                rb.linearVelocity = Vector2.zero;
+                rb.AddForce( new Vector2 (0, jumpForce));
+            }
+        }
+    }
+}
